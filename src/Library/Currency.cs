9176ff4eed885f5ab97249319984577ff25782d0
Luciana Diaz <ldiaz2511@gmail.com>
2021-06-21 23:20:06 -0300

    se modifica clases Currency y CurrencyExchanger

--- src/Library/Currency.cs
+++ src/Library/Currency.cs
@@ -2,13 +2,22 @@
 
 namespace Library
 {
+    //Esta cumple con el patrón Expert ya que es la que posee toda la información sobre Currency.
+    //Además también cumple con el patrón SRP por tener una unica razón de cambio.
+    
+
+    /// <summary>
+    /// Clase correspondiente al tipo de moneda
+    /// </summary>
     public class Currency
     {
-        public string CurrencyType
+        public string Type { get; set; }
+        public double ExchangeRate { get; set; }
+
+        public Currency(string type, double rate)
         {
-            get;
-            private set;
+            this.Type = type;
+            this.ExchangeRate = rate;
         }
-        
     }
 }
