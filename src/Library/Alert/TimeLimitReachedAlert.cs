8c456da3329f8e28458bc5a253442748febee2e0
antoniamescia <antonia.mescia.novarese@gmail.com>
2021-06-23 22:06:34 -0300

    Justificaciones de principios y patrones en carpeta Alert

--- src/Library/Alert/TimeLimitReachedAlert.cs
+++ src/Library/Alert/TimeLimitReachedAlert.cs
@@ -3,7 +3,15 @@
 namespace Library
 {
     public class TimeLimitReachedAlert : IAlert
-    {
+    { 
+        /*
+        Patrones y principios:
+        Cumple con SRP porque solo se identifica una razón de cambio: algún cambio en la lógica del método SendAlert.
+        Cumple con LSP porque el tipo implícito que define la clase puede ser sustiuido por IAlert.
+        Cumple con ISP porque solo implementa una interfaz (IAlert).
+        Cumple con Expert porque tiene toda la información necesaria para poder cumplir con la responsabilidad de enviar la alerta.
+        Cumple con Polymorphism porque usa el método polimórfico SendAlert.
+        */
         public string SendAlert(Account account)
         {
            if ((account.MaxGoal.TimeLimit - DateTime.Today).TotalDays < 0 && account.Amount < account.MaxGoal.ObjectiveAmount)
