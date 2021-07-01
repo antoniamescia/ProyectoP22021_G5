44f3d6939507dbc5d2d8a8fd36e3dd539e399aa4
FacundoAcostaG <80794469+FacundoAcostaG@users.noreply.github.com>
2021-07-01 20:24:03 -0300

    Se refencia la api de html

--- src/Library/Currency/CurrencyExchanger.cs
+++ src/Library/Currency/CurrencyExchanger.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Library
+namespace BankerBot
 {
     public class CurrencyExchanger
     {
--- src/Library/EndUser.cs
+++ src/Library/EndUser.cs
@@ -2,7 +2,7 @@
 using System.Text;
 using System.Collections.Generic;
 
-namespace Library
+namespace BankerBot
 {
     public class EndUser
     {
