c2b3ecbede5d4e89610a1e1c4c685d7a05b49917
Luciana Diaz <ldiaz2511@gmail.com>
2021-07-10 19:34:59 -0300

    Fixed Test

--- src/Test/Library.Test/EndUserTest.cs
+++ src/Test/Library.Test/EndUserTest.cs
@@ -1,116 +1,117 @@
-// using NUnit.Framework;
-// using System;
-// using System.Collections.Generic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
-// namespace BankerBot.Test
-// {
-//     public class EndUserTest
-//     {
-//         private EndUser facundo;
-//         private Currency currency;
+namespace BankerBot.Test
+{
+    public class EndUserTest
+    {
+        private User facundo;
+        private Currency currency;
+        private Type type;
 
-//         [SetUp]
-//         public void Setup()
-//         {
-//             facundo = new EndUser("Facundo", "1234");
-//             currency = new Currency("UYU", 1);
-//             facundo.AddAccount("BBVA", currency, 300, new SavingsGoal(800, currency, new DateTime(2021,06,25)), new SavingsGoal(200, currency, new DateTime(2021,06,25)));
-//         }
+        [SetUp]
+        public void Setup()
+        {
+            facundo = new User("Facundo", "1234");
+            currency = new Currency("UYU", "UY$", 1);
+            this.type = new Type ();
+            facundo.AddAccount(type, "BBVA", currency, 300, new SavingsGoal(800, 200));
+        }
 
-//         [Test]
-//         public void TestAccountIsAddedAndThenRemoved() //Prueba que se agrega una cuenta al usuario y luego se saca
-//         {
-//             int initialQuantity = facundo.Accounts.Count;
+        [Test]
+        public void TestAccountIsAddedAndThenRemoved() //Prueba que se agrega una cuenta al usuario y luego se saca
+        {
+            int initialQuantity = facundo.Accounts.Count;
 
-//             facundo.AddAccount("ITAU", currency, 300, new SavingsGoal(800, currency, new DateTime(2021,06,25)), new SavingsGoal(200, currency, new DateTime(2021,06,25)));
+            facundo.AddAccount(type, "ITAU", currency, 300, new SavingsGoal(800, 200));
 
-//             int secondQuantity = facundo.Accounts.Count;
+            int secondQuantity = facundo.Accounts.Count;
 
-//             facundo.RemoveAccount(facundo.Accounts[1]);
+            facundo.RemoveAcount(facundo.Accounts[1]);
             
-//             Assert.AreNotEqual(initialQuantity, secondQuantity);
-//             Assert.AreNotEqual(secondQuantity, facundo.Accounts.Count);
-//         }
+            Assert.AreNotEqual(initialQuantity, secondQuantity);
+            Assert.AreNotEqual(secondQuantity, facundo.Accounts.Count);
+        }
 
-//         [Test]
-//         public void TestAccountCantBeRepeatedByName() //Prueba que no permite agregar una nueva cuenta con un nombre ya existente
-//         {
-//             int cant = facundo.Accounts.Count;
+        [Test]
+        public void TestAccountCantBeRepeatedByName() //Prueba que no permite agregar una nueva cuenta con un nombre ya existente
+        {
+            int cant = facundo.Accounts.Count;
 
-//             facundo.AddAccount("BBVA", currency, 400, new SavingsGoal(900, currency, new DateTime(2021,06,25)), new SavingsGoal(200, currency, new DateTime(2021,06,25)));
+            facundo.AddAccount(type, "BBVA", currency, 400, new SavingsGoal(900, 200));
             
-//             Assert.AreEqual(cant, facundo.Accounts.Count);
-//         }
+            Assert.AreEqual(cant, facundo.Accounts.Count);
+        }
 
-//         [Test]
-//         public void TestUsernameChanges()       //Prueba que se modifica el usuario
-//         {
-//             string name = facundo.Username;
+        [Test]
+        public void TestUsernameChanges()       //Prueba que se modifica el usuario
+        {
+            string name = facundo.Username;
 
-//             facundo.ChangeUsername("Felipe");
+            facundo.ChangeUsername("Felipe");
 
-//             Assert.AreNotEqual(name, facundo.Username);
+            Assert.AreNotEqual(name, facundo.Username);
 
-//         }
+        }
 
-//         [Test]
-//         public void TestPasswordChanges()       //Prueba que se modifica la contraseña
-//         {
-//             string pass = facundo.Password;
+        [Test]
+        public void TestPasswordChanges()       //Prueba que se modifica la contraseña
+        {
+            string pass = facundo.Password;
 
-//             facundo.ChangePassword("4321");
+            facundo.ChangePassword("4321");
 
-//             Assert.AreNotEqual(pass, facundo.Password);
-//         }
+            Assert.AreNotEqual(pass, facundo.Password);
+        }
 
-//         [Test]
-//         public void TestCategoryIsAddedAndThenRemoved() //Prueba que se agrega una categoria y luego se saca
-//         {
-//             int initialQuantity = facundo.ExpenseCategories.Count;
+        [Test]
+        public void TestCategoryIsAddedAndThenRemoved() //Prueba que se agrega una categoria y luego se saca
+        {
+            int initialQuantity = facundo.ExpenseCategories.Count;
 
-//             facundo.AddExpenseCategory("Comida");
+            facundo.AddExpenseCategory("Rifa");
 
-//             int secondQuantity = facundo.ExpenseCategories.Count;
+            int secondQuantity = facundo.ExpenseCategories.Count;
 
-//             facundo.RemoveExpenseCategory("Comida");
+            facundo.RemoveExpenseCategory("Rifa");
 
-//             Assert.AreNotEqual(initialQuantity, secondQuantity);
-//             Assert.AreNotEqual(secondQuantity, facundo.ExpenseCategories.Count);
-//         }
+            Assert.AreNotEqual(initialQuantity, secondQuantity);
+            Assert.AreNotEqual(secondQuantity, facundo.ExpenseCategories.Count);
+        }
 
-//         [Test]
-//         public void TestCantAddRepeatedExpenseCategory() //Prueba que no se puede agregar categorias repetidas, sin importar si esta en mayuscula o minuscula
-//         {
-//             facundo.AddExpenseCategory("Ropa");
-//             facundo.AddExpenseCategory("Comida");
+        [Test]
+        public void TestCantAddRepeatedExpenseCategory() //Prueba que no se puede agregar categorias repetidas, sin importar si esta en mayuscula o minuscula
+        {
+            facundo.AddExpenseCategory("Rifa");
+            facundo.AddExpenseCategory("Salud");
 
-//             int expectedQuantity = facundo.ExpenseCategories.Count;
+            int expectedQuantity = facundo.ExpenseCategories.Count;
 
-//             facundo.AddExpenseCategory("ropa");
-//             facundo.AddExpenseCategory("ROPA");
+            facundo.AddExpenseCategory("rifa");
+            facundo.AddExpenseCategory("RIFA");
 
-//             Assert.AreEqual(expectedQuantity, facundo.ExpenseCategories.Count);
-//         }
+            Assert.AreEqual(expectedQuantity, facundo.ExpenseCategories.Count);
+        }
 
-//         [Test]
-//         public void TestDisplayAccountsReturnsTextAsExpected() //Prueba que DisplayAccounts devuelve el texto esperado
-//         {
-//             facundo.AddAccount("ITAU", currency, 300, new SavingsGoal(800, currency, new DateTime(2021,06,25)), new SavingsGoal(200, currency, new DateTime(2021,06,25)));
+        [Test]
+        public void TestDisplayAccountsReturnsTextAsExpected() //Prueba que DisplayAccounts devuelve el texto esperado
+        {
+            facundo.AddAccount(type, "ITAU", currency, 300, new SavingsGoal(800, 200));
+            string expectedResult = "1 - BBVA\n2 - ITAU\n";
 
-//             string expectedResult = "BBVA\nITAU\n";
+            Assert.AreEqual(expectedResult, facundo.DisplayAccounts());
+        }
 
-//             Assert.AreEqual(expectedResult, facundo.DisplayAccounts());
-//         }
+        [Test]
+        public void TestDisplayExpenseCategoriesReturnsTextAsExpected() //Prueba que DisplayExpenseCategories devuelve el texto esperado
+        {
+            facundo.AddExpenseCategory("Rifa");
+            facundo.AddExpenseCategory("Salud");
 
-//         [Test]
-//         public void TestDisplayExpenseCategoriesReturnsTextAsExpected() //Prueba que DisplayExpenseCategories devuelve el texto esperado
-//         {
-//             facundo.AddExpenseCategory("Ropa");
-//             facundo.AddExpenseCategory("Comida");
+            string expectedResult = "1 - Comida\n2 - Ropa\n3 - Alquiler\n4 - Pagos fijos\n5 - Tarjetas\n6 - Luz\n7 - Transporte\n8 - Agua\n9 - Mascota\n10 - Regalos\n11 - Diversión\n12 - Rifa\n13 - Salud\n"; 
 
-//             string expectedResult = "Ropa\nComida\n";
-
-//             Assert.AreEqual(expectedResult, facundo.DisplayExpenseCategories());
-//         }
-//     }
-// }
+            Assert.AreEqual(expectedResult, facundo.DisplayExpenseCategories());
+        }
+    }
+}
